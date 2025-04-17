4175848fe0fe80a0aa38d1a2bb3787577d1df704
MAIDUGU-jean1 <maidugujeandedieu600@gmail.com>
2025-04-17 01:16:51 +0100

    Added shop section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,33 @@ function toggleMenu() {
     const navMenu = document.getElementById("nav-menu");
     navMenu.classList.toggle("active");
   }
+
+
+  const cart = [];
+
+
+  document.querySelectorAll('.add-to-cart').forEach((button, index) => {
+    button.addEventListener('click', () => {
+ 
+      const card = button.parentElement;
+      const name = card.querySelector('.product-name').textContent;
+      const price = card.querySelector('.product-price').textContent;
+
+
+      cart.push({ name, price });
+
+      console.log("Cart:", cart);
+      alert(`${name} added to cart!`);
+    });
+  });
+
+
+  const links = document.querySelectorAll('.nav-link');
+  const currentPage = window.location.pathname.split("/").pop(); // gets current page file
+
+  links.forEach(link => {
+    const href = link.getAttribute('href');
+    if (href === currentPage) {
+      link.classList.add('active');
+    }
+  });
